506ff1852947232407258bdba56b8a982d207a1e
zhoupeican <zpc20106200@gmail.com>
2019-12-04 21:42:29 +0800

    添加配置文件

---
+++ www/config_default.py
@@ -0,0 +1,15 @@
+# _*_ conding: utf-8 -*-
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'www',
+        'password': 'www',
+        'db': 'awesome'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
